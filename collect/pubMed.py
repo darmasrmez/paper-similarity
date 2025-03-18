4c08fee1194217797ffb3c5820c25a5a042362a3
Karlah <126218502+Karlah-0@users.noreply.github.com>
2025-03-17 22:18:51 -0600

    Update pubMed.py

--- collect/pubMed.py
+++ collect/pubMed.py
@@ -1,4 +1,61 @@
 import asyncio
 import aiohttp
 import requests
+import re
+import csv
+import random
+import time
 from bs4 import BeautifulSoup
+
+BASE_URL = 'https://pubmed.ncbi.nlm.nih.gov/'
+
+async def extract_info(pmid, session):
+    url = f"{BASE_URL}{pmid}/?format=pubmed"
+    async with session.get(url) as response:
+        soup = BeautifulSoup(await response.text(), 'html.parser')
+        
+        def extract(pattern, text, join=False):
+            match = re.findall(pattern, text)
+            return ', '.join(match) if join and match else (match[0] if match else None)
+        
+        data = {
+            'DOI': extract(r"(LID|AID)\s+-\s+(.+)\s+\[doi\]", soup.text),
+            'Title': extract(r"TI  -\s*(.+(?:\n\s{6,}.+)*)", soup.text),
+            'Authors': extract(r"FAU\s+-\s+(.+)\r", soup.text, join=True),
+            'Abstract': extract(r"AB  -\s*(.+(?:\n\s{6,}.+)*)", soup.text),
+            'Journal': extract(r"JT  -\s*(.+)\r", soup.text),
+            'Date': extract(r"DP  -\s*(\d{4} \w{3}(?: \d{1,2})?)", soup.text),
+        }
+        return data if all(data.values()) else None
+
+async def get_pubmed_articles(num_articles):
+    articles = []
+    page = 1
+    async with aiohttp.ClientSession() as session:
+        while len(articles) < num_articles:
+            url = f"{BASE_URL}trending/?filter=simsearch1.fha&page={page}"
+            soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+            pmids = [tag['href'].split('/')[-2] for tag in soup.find_all('a', class_='docsum-title')]
+            
+            tasks = [extract_info(pmid, session) for pmid in pmids]
+            results = await asyncio.gather(*tasks)
+            articles.extend(filter(None, results))
+            
+            page += 1
+            time.sleep(random.uniform(1, 2))
+            
+    return articles[:num_articles]
+
+async def save_articles(articles, filename):
+    with open(filename, 'w', encoding='utf-8', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=articles[0].keys())
+        writer.writeheader()
+        writer.writerows(articles)
+    print(f'Los artículos se han guardado en {filename}')
+
+if __name__ == '__main__':
+    num_articles = 300
+    articles = asyncio.run(get_pubmed_articles(num_articles))
+    if articles:
+        asyncio.run(save_articles(articles, 'pubmed_raw_corpus.csv'))
+
