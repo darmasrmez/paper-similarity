9a42110baac863f696a7170e096fc79b6cc88237
Karlah <126218502+Karlah-0@users.noreply.github.com>
2025-04-01 08:56:05 -0600

    Update functions.py

--- app/functions.py
+++ app/functions.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 def load_vectorized_corpus(corpus_path):
     with open(corpus_path, 'rb') as f:
